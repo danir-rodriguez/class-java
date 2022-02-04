1a9582239420cb643db4160591a69188de2d6938
Juan Jose Hernandez <ju16jo@gmail.com>
2022-02-04 13:23:55 -0600

    initial commit

---
+++ src/NamingJava.java
@@ -0,0 +1,28 @@
+             //Upper Camel Case
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33337777;
+        int celPhone = 55553333;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "Mxn";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Irene Anahi Salgaado Diaz de la Vega";
+        int zizeInCentimeters = 26;
+
+
+
+
+
+    }
+}
